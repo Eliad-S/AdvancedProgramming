1db5e589c20235eb3d121e50fd9a4c9ed8286433
shirgoldberger <shirgoldberger@gmail.com>
2019-12-28 22:59:33 +0200

    28.12

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 #include "Lexer.h"
 
 int main(int argc, char *argv[]) {
-  ifstream inFile(argv[1], ios::in);
-  Lexer lexer(argv[1]);
-  InterpreterFlight *interpreter_flight = InterpreterFlight::getInstance();
-  interpreter_flight->setTokens(lexer.token);
-  interpreter_flight->parser();
-  return 0;
+    (void) argc;
+    ifstream inFile(argv[1], ios::in);
+    Lexer lexer(argv[1]);
+    InterpreterFlight *interpreter_flight = InterpreterFlight::getInstance();
+    interpreter_flight->setTokens(lexer.token);
+    interpreter_flight->parser();
+    return 0;
 }
